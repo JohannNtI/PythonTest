ad798939d9f00739858cc4ce282b007f74af7842
Johann <johan.suwafranco@elev.ga.ntig.se>
2024-10-21 14:21:29 +0200

    management

--- Madlibs.py
+++ Madlibs.py
@@ -1,35 +1,52 @@
 import time
 
-print ("hej hopp")
 
-# Introduktion
+# Introduction
 print ("Welcome to Madlibs!")
-print ("You're going to be presented with a text on what type of word you are supposed to write in. Colours, verbs, nouns etc. Enjoy!")
-
+time.sleep(2)
+print ("youre going to write in the missings words of this text.")
+time.sleep(1.5)
+print ("You're going to be soon presented with a text on what type of word you are supposed to write in. Colours, verbs, nouns etc. Enjoy!")
+time.sleep(5)
 print ("It was about ________________ ________________ ago when I came to the hospital in a ________________.The hospital is a/an ________________ place, there are a lot of ________________ ________________here.There are nurses here who have ______________ ________________. If someone wants to come into my roomI told them that they have to ________________ first. I have decorated my room with ________________________________. Today a doctor came into my room and they were wearing a ________________ on their________________. I heard that all doctors ________________ ________________ every day for breakfast. Themost ________________ thing about being in the hospital is the ________________ ________________!")
+time.sleep(10)
 
-
-
-
-
-
+#Questions player is supposed to answer.
+Numer_1 = input(print("write a Number: "))
+Tid = input(print("write a measure of Time: "))
+Transport = input(print("write a mode of Transportation: "))
+Adjektiv_1 = input(print("write an Adjective: "))
+Adjektiv_2 = input(print("write another Adjective: "))
+Substantiv = input(print("write a Noun: "))
+Färg = input(print("write a Colour: "))
+Kroppsdel_l = input(print("write a body part: "))
+Verb_1 = input(print("write a Verb: "))
+Numer_2 = input(print("write a Number: "))
+Substantiv_2 = input(print("write a Noun: "))
+Substantiv_3 = input(print("write another Noun: "))
+Kroppsdel_2 = input(print("write another body part: "))
+Verb_2 = input(print("write another Verb: "))
+Substantiv_4 = input(print("write another Noun: "))
+Adjektiv_3 = input(print("write another Adjective: "))
+Rolig_Ord = input(print("write a funny word: "))
+Substantiv_5 = input(print("write the final Noun: ")) 
 
 #Ord lista
-Numer = input("")
+Numer_1 = input("")
 Tid = input("")
 Transport = input("")
 Adjektiv_1 = input("")
 Adjektiv_2 = input("")
-Substantiv = input("")
+Substantiv_1 = input("")
 Färg = input("")
-Kroppsde_l = input("")
+Kroppsdel_l = input("")
 Verb_1 = input("")
 Numer_2 = input("")
 Substantiv_2 = input("")
 Substantiv_3 = input("")
 Kroppsdel_2 = input("")
 Verb_2 = input("")
-Sunstantiv_4 = input("")
+Substantiv_4 = input("")
 Adjektiv_3 = input("")
 Rolig_Ord = input("")
 Substantiv_5 = input("")
